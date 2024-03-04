b1f374314dc53f3a78cac367986758d663354ffe
Manuel-d77 <52802322+Manuel-d77@users.noreply.github.com>
2024-03-04 18:32:10 +0000

    Adding request and response files

---
+++ src/ECommerce.CartExperience/ECommerce.CartExperience.Api/CartItemRemovalResponse.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.CartExperience.Api.Models;
+
+namespace ECommerce.CartExperience.Api
+{
+    public class CartItemRemovalResponse
+    {
+        public CartItem? CartItem { get; set; }
+
+        public bool IsFoundAndDeleted { get; set; }
+    }
+}
---
+++ src/ECommerce.CartExperience/ECommerce.CartExperience.Api/CartRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ECommerce.CartExperience.Api
+{
+    public class CartRequest
+    {
+        public string PhoneNumber { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
